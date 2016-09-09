4ad93a08afc951a94f554e497b1ad713332a0fde
akihiro-tachibana <akihiro.tachibana@f.flect.co.jp>
2016-09-09 17:06:58 +0900

    googlemap実装

--- src/main/java/cariot/jp/controller/IndexController.java
+++ src/main/java/cariot/jp/controller/IndexController.java
@@ -3,9 +3,6 @@ package cariot.jp.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
- * Created by akihiro.tachibana on 2016/09/09.
- */
 @Controller
 public class IndexController {
 
